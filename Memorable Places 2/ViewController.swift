7f8cf28348e6a0e15d2167b11ca6df6d2128e4a5
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-09 15:13:29 -0500

    Initial Commit

---
+++ Memorable Places 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorable Places 2
+//
+//  Created by Maalik Hornbuckle on 1/9/17.
+//  Copyright © 2017 Maalik Hornbuckle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
